bbd63b2d3df9302dba5a28ad938207ca600bdb46
Silvanos Eric <ericoderito@gmail.com>
2024-07-18 13:14:15 +0300

    Refactor movie card display and add ticket purchase functionality
    - Update `tickets_sold` in `db.json` for data consistency
    - Implement ticket purchase initialization in `script.js`
    - Change `createMovieCardEl` to `createSelectedMovieCard` for clarity, include ticket purchase logic
    - Introduce utility functions `allowBuyTicket`, `convertToHoursAndMinutes`, and `patchMovie` for
      ticket buying
    - Modify movie card structure in `script.js` for better UI feedback on ticket availability
    - Adjust CSS to enhance cursor style on movie items for improved user interaction

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const movieMenuEl = document.querySelector("ul#films");
 async function main() {
   showFirstMovieDetails();
   showMovieMenu();
+  initializeTicketPurchase();
 }
 
 // Utility functions
@@ -41,14 +42,17 @@ async function fetchMovies() {
 }
 
 function displayMovie(movie) {
-  const movieEl = createMovieCardEl(movie);
+  const movieEl = createSelectedMovieCard(movie);
   featuredMovieEl.appendChild(movieEl);
 }
 
-function createMovieCardEl(movie) {
+function createSelectedMovieCard(movie) {
+  const ableToBuyTicket = allowBuyTicket(movie.capacity, movie.tickets_sold);
+
   const movieEl = document.createElement("div");
   movieEl.classList.add("card");
   movieEl.style.width = "18rem";
+  movieEl.dataset.id = movie.id;
 
   const img = document.createElement("img");
   img.setAttribute("src", movie.poster);
@@ -68,12 +72,46 @@ function createMovieCardEl(movie) {
   p.classList.add("card-text");
   body.appendChild(p);
 
+  const button = document.createElement("button");
+  if (ableToBuyTicket) {
+    button.textContent = "Buy Ticket";
+    button.classList.add("btn", "btn-success");
+    button.setAttribute("id", "purchase-ticket");
+  } else {
+    button.classList.add("btn", "btn-secondary");
+    button.textContent = "Sold Out";
+    button.setAttribute("disabled", true);
+  }
+  body.appendChild(button);
+
   const footer = document.createElement("div");
-  footer.textContent = `Available tickets: ${availableTickets(
-    movie.capacity,
-    movie.tickets_sold
-  )}`;
   footer.classList.add("card-footer");
+
+  const ul = document.createElement("ul");
+  ul.classList.add("list-group");
+
+  const runtimeEl = document.createElement("li");
+  runtimeEl.textContent = `Runtime: ${convertToHoursAndMinutes(movie.runtime)}`;
+  runtimeEl.classList.add("list-group-item");
+  ul.appendChild(runtimeEl);
+
+  const showTimeEl = document.createElement("li");
+  showTimeEl.textContent = `Showtime: ${movie.showtime}`;
+  showTimeEl.classList.add("list-group-item");
+  ul.appendChild(showTimeEl);
+
+  if (ableToBuyTicket) {
+    const availableTicketsEl = document.createElement("li");
+    availableTicketsEl.textContent = `Available tickets: ${availableTickets(
+      movie.capacity,
+      movie.tickets_sold
+    )}`;
+    availableTicketsEl.classList.add("list-group-item", "available-tickets");
+    availableTicketsEl.dataset.tickets_sold = movie.tickets_sold;
+    ul.appendChild(availableTicketsEl);
+  }
+
+  footer.appendChild(ul);
 
   movieEl.append(img, body, footer);
 
@@ -101,7 +139,7 @@ function createMovieMenu(movies) {
 
 function createMovieMenuItem(movie) {
   const menuItem = document.createElement("li");
-  menuItem.classList.add("list-group-item", "d-flex");
+  menuItem.classList.add("movie-item", "list-group-item", "d-flex");
 
   const p = document.createElement("p");
   p.classList.add("ms-2");
@@ -123,6 +161,77 @@ function displayMovieMenu() {
 function addCollectionToFragmentEl(collection) {
   for (const item of collection) {
     fragmentEl.appendChild(item);
+  }
+}
+
+function allowBuyTicket(capacity, tickets_sold) {
+  const remainingTickets = availableTickets(capacity, tickets_sold);
+  if (remainingTickets <= 0) {
+    return false;
+  }
+  return true;
+}
+
+function convertToHoursAndMinutes(totalMinutes) {
+  const hours = Math.floor(totalMinutes / 60);
+  const minutes = totalMinutes % 60;
+  return `${hours} ${hours > 1 ? "hours" : "hour"} and ${minutes} ${
+    minutes > 1 ? "minutes" : "minute"
+  }`;
+}
+
+function initializeTicketPurchase() {
+  featuredMovieEl.addEventListener("click", (event) => {
+    if (event.target.matches("button#purchase-ticket")) {
+      const cardBody = event.target.parentElement;
+      const card = cardBody.parentElement;
+      const movieId = card.dataset.id;
+
+      const availableTicketsEl = card.querySelector("li.available-tickets");
+      const ticketsSoldBeforePurchase = Number.parseInt(
+        availableTicketsEl.dataset.tickets_sold
+      );
+
+      const ticketsSoldAfterPurchase = ticketsSoldBeforePurchase + 1;
+
+      updateMovie(movieId, ticketsSoldAfterPurchase);
+    }
+  });
+}
+
+async function updateMovie(movieId, newTicketsSold) {
+  const updateMovie = await patchMovie(movieId, newTicketsSold);
+  console.log(updateMovie);
+}
+
+async function patchMovie(movieId, newTicketsSold) {
+  console.log(newTicketsSold);
+  try {
+    // Fetch user data from the API
+    const response = await fetch(`http://localhost:3000/movies/${movieId}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        tickets_sold: newTicketsSold,
+      }),
+    });
+
+    // Check if the response is okay
+    if (!response.ok) {
+      throw new Error(`Network response was not ok ${response.statusText}`);
+    }
+
+    // Parse the JSON data
+    const movieData = await response.json();
+
+    // return the films data
+    return movieData;
+  } catch (error) {
+    // Handle any errors that occurred during the fetch
+    console.error(`There has been an error with your fetch operation ${error}`);
   }
 }
 
